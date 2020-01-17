61cab2ae41d92cf47d5fcc1c0a509353e13bbc01
chendf999 <chendf999@gmail.com>
2020-01-17 17:02:06 +0800

    adjust card distance

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ let mainCards = []
 let methodCards = []
 
 function generateCards(num, cards){
-  for (var i = 3; i <= num; i++) {
+  for (var i = 1; i <= num; i++) {
     if ( i % 2 !== 0){
       cards.push({
         front: i, back: i+1
@@ -18,7 +18,7 @@ $(document).ready(function() {
 
   mainCards.map((item,i) => {
     mainCards[i].id = 'main-' + i;
-    let left = 20+30*i;
+    let left = 20+51*i;
     let div = `<div class="card ${item.id}"  style="position:absolute;left:${left}px">
     <img class="back" src="./main-cards/maincard${item.back}.jpg">
     <img class="front" src="./main-cards/maincard${item.front}.jpg">
@@ -30,7 +30,7 @@ $(document).ready(function() {
 
   methodCards.map((item,i) => {
     methodCards[i].id = 'method-' + i;
-    let left = 20+20*i;
+    let left = 20+25*i;
     let div = `<div class="card ${item.id}" style="position:absolute;left:${left}px">
     <img class="back" src="./method-cards/methodcard${item.back}.jpg">
     <img class="front" src="./method-cards/methodcard${item.front}.jpg">
